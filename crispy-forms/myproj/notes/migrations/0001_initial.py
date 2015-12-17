423da755721f7d8230cf5ae1590e1aa13917c3f8
Victor Menezes <menezes.victor@gmail.com>
2015-12-17 11:56:18 -0500

    init crispy sample

---
+++ crispy-forms/myproj/notes/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Note',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('title', models.CharField(max_length=250)),
+                ('content', models.TextField()),
+                ('status', models.IntegerField(choices=[(0, 'Past'), (1, 'Active'), (2, 'Cancelled'), (3, 'Done'), (4, 'Lost')], default=1)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
